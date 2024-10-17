599c831206ae59c676179b570a9ddfc6f8f7115f
Adriana Torres <aditorn@ymail.com>
2024-10-17 16:31:49 -0500

    Abordamos operaciones aritmeticas en pandas

---
+++ operaciones_aritmeticas_en_dataframes.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+datos1 = {'A': [1, 2, 3], 'B': [4, 5, 6]}
+
+df1 = pd.DataFrame(datos1)
+
+
+#sunmar 10 a todas las entradas
+df_suma = df1 + 10
+print(df_suma)
+
+serie = pd.Series([10, 20], index = ['A', 'B'])
+
+#Sumar la serie al dataframe
+resultado = df1 + serie
+print(resultado)
+
+datos2 = {'A': [10, 20, 30], 'B': [40, 50, 60]}
+df2 = pd.DataFrame(datos2)
+
+#Sumar ambos dataframes
+df_suma2 = df1 + df2
+print(df_suma2)
+

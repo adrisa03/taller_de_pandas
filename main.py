280637170223bc64a7f95c84b44c5fe02bbfd35f
Adriana Torres <aditorn@ymail.com>
2024-10-17 15:53:23 -0500

    Abordamos indexacion avanzada con pandas

--- main.py
+++ main.py
@@ -9,8 +9,24 @@ print (serie)
 datos = {'Nombre': ['Alice', 'Bob', 'Claire'],
          'Edad': [25,30,35],
          'Ciudad': ['A', 'B', 'C']}
-df = pd.DataFrame(datos)
+df = pd.DataFrame(datos, index = ['a', 'b', 'c'])
 print (df)
+#Selccionar datos con loc
+seleccion_loc = df.loc['b', 'Edad']
+print (seleccion_loc) #RESULTADO 30
+
+#Seleccionar datos con iloc
+seleccion_iloc = df.iloc[1, 1]
+print (seleccion_iloc) #RESULTADO 30
+
+#Seleccionar datos con at
+seleccion_at = df.at['b', 'Edad']
+print (seleccion_at) #RESULTADO 30
+
+#Seleccionar datos con iat
+seleccion_iat = df.iat[1, 1]
+print (seleccion_iat) #RESULTADO 30
+
 
 data = {'state': ['Ohio', 'Ohio', 'Ohio', 'Nevada', 'Nevada', 'Nevada'],
         'year': [2000, 2001, 2002, 2001, 2002, 2003],
@@ -25,3 +41,5 @@ frame2 = pd.DataFrame(data, columns=['year', 'state', 'pop', 'debt'],
                        index=['one', 'two', 'three', 'four', 'five', 'six'])
 print (frame2)
 print (frame2.columns)
+print(frame2['state'])
+print (frame2.year)
